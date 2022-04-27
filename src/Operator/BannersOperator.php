f37cadc3ad222d013411796bf02b7a8dabf0e781
Bartosz Wojdałowicz <bartoszwojdalowicz@gmail.com>
2022-04-27 15:07:06 +0200

    refactor

--- src/Operator/BannersOperator.php
+++ src/Operator/BannersOperator.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
 declare(strict_types=1);
 
 namespace BitBag\SyliusBannerPlugin\Operator;
@@ -8,14 +15,13 @@ use BitBag\SyliusBannerPlugin\Entity\AdInterface;
 use BitBag\SyliusBannerPlugin\Entity\BannerInterface;
 use Doctrine\Common\Collections\Collection;
 
-class BannersOperator implements BannersOperatorInterface
+final class BannersOperator implements BannersOperatorInterface
 {
     public function operate(
         AdInterface $ad,
         string $sectionCode,
         string $localeCode
-    ): ?array
-    {
+    ): ?array {
         /** @var Collection $adBanners */
         $adBanners = $ad->getBanners();
         if (true === $adBanners->isEmpty()) {
--- src/Serializer/SectionSerializer.php
+++ src/Serializer/SectionSerializer.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
 declare(strict_types=1);
 
 namespace BitBag\SyliusBannerPlugin\Serializer;
@@ -13,7 +20,7 @@ use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
 
-class SectionSerializer implements
+final class SectionSerializer implements
     ContextAwareNormalizerInterface,
     NormalizerAwareInterface
 {

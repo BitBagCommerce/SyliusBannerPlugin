7156d8ecbc55ccf108fd1d668af50469733b1b89
Bartosz Wojdałowicz <bartoszwojdalowicz@gmail.com>
2022-05-04 11:07:15 +0200

    CR fixes

--- src/Controller/GetAdsBanners.php
+++ src/Controller/GetAdsBanners.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
 declare(strict_types=1);
 
 namespace BitBag\SyliusBannerPlugin\Controller;
@@ -8,7 +15,7 @@ use BitBag\SyliusBannerPlugin\Command\GetAdsBanners as GetAdsBannersCommand;
 use BitBag\SyliusBannerPlugin\Operator\BannersOperatorInterface;
 use BitBag\SyliusBannerPlugin\Repository\AdRepositoryInterface;
 
-class GetAdsBanners
+final class GetAdsBanners
 {
     private AdRepositoryInterface $adRepository;
 

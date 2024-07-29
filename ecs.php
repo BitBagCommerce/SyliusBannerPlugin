60f20b5de13c9ae33ef2c9e1990dee2b4180c415
Krzysztof Odważny <christopherheroo@gmail.com>
2024-07-30 01:05:41 +0200

    upgrade to sylius 1.13

--- ecs.php
+++ ecs.php
@@ -2,19 +2,11 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import('vendor/bitbag/coding-standard/ecs.php');
+return static function (ECSConfig $config): void {
+    putenv('ALLOW_BITBAG_OS_HEADER=1');
 
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PATHS, [
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-    ]);
-    $parameters->set(Option::SKIP, [
-        __DIR__ . '/tests/Application/var',
-        __DIR__ . '/vendor'
-    ]);
+    $config->import('vendor/bitbag/coding-standard/ecs.php');
+    $config->paths(['src', 'tests']);
 };
